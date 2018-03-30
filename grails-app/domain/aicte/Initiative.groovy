631aaa889d6015c724843dc4946eb6eb5cadeaff
Aditya Gurjar <adityaadi1467@gmail.com>
2018-03-30 17:41:13 +0530

    Added desc

--- grails-app/domain/aicte/Initiative.groovy
+++ grails-app/domain/aicte/Initiative.groovy
@@ -2,6 +2,7 @@ package aicte
 
 class Initiative {
     String name
+    String desc
     Date date = new Date()
     static belongsTo = [owner: User]
     static hasMany = [beneficiaries: Beneficiary,ratings:Rating,parameters:Parameter]
